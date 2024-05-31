cca1996b4f4fbbf921dc230a476bd1383506a461
nikunj63 <nikunjs6324@gmail.com>
2024-05-31 23:43:28 +0530

    food_identifier_app

---
+++ food_identifier_app/android/app/src/main/kotlin/com/example/food_identifier_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.food_identifier_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ food_identifier_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:food_identifier_app/pages/splash_screen.dart';
+import 'package:food_identifier_app/services/name_save.dart';
+
+Future<void>main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await NameSave().writeFirstSave();
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food Identifier App',
+      theme : ThemeData(fontFamily:'Italiana'),
+      home: SplashScreen(),
+    );
+  }
+}
